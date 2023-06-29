cd196d80baa82e3a5ad27e17a54c0508f374eecd
lukaszbalowski <l.balowski@gmail.com>
2023-06-29 22:01:52 +0200

    - Display sum of bags

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -1,5 +1,6 @@
 package pl.coderslab.charity.controller;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +14,20 @@ import java.util.List;
 public class HomeController {
 
     private final pl.coderslab.charity.repository.InstitutionRepository InstitutionRepository;
+    private final JdbcTemplate jdbcTemplate;
 
 
-    public HomeController(InstitutionRepository InstitutionRepository) {
+    public HomeController(InstitutionRepository InstitutionRepository, JdbcTemplate jdbcTemplate) {
         this.InstitutionRepository = InstitutionRepository;
+        this.jdbcTemplate = jdbcTemplate;
     }
     @RequestMapping("/")
     public String homeAction(Model model){
         List<Institution> institutions = InstitutionRepository.findAll();
         model.addAttribute("institutions", institutions);
+
+        Integer sumOfBags = jdbcTemplate.queryForObject("SELECT SUM(quantity) FROM donations", Integer.class);
+        model.addAttribute("sumOfBags", sumOfBags);
         return "index";
     }
 }
